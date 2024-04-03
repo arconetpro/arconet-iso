3570097f45c03b62c3859f0e627d04200d94a264
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-03 22:52:02 +0200

    first commit

---
+++ archiso/profiledef.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="arcolinuxl-git"
+iso_label="arcolinuxl-git-v24.05.01"
+iso_publisher="ArcoLinux <http://www.arcolinux.info>"
+iso_application="ArcoLinux Live/Rescue CD"
+iso_version="v24.05.01"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/gshadow"]="0:0:600"
+  ["/etc/shadow"]="0:0:600"
+  ["/root"]="0:0:700"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+  ["/usr/local/bin/arcolinux-snapper"]="0:0:755"
+)
