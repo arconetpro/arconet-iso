61c2c70ea01f230d183ac7047b3f742a0afdc8b1
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-03 06:37:28 +0200

    kvantum-qt5

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -20,6 +20,7 @@ file_permissions=(
   ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:600"
   ["/root"]="0:0:700"
+  ["/root/.gnupg"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
