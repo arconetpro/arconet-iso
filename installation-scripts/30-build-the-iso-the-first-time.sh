c882cb39dddcef57d5787cb125e293422905f92a
Erik Dubois <erik.dubois@gmail.com>
2024-10-29 06:39:30 +0100

    gnu-netcat

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -66,7 +66,7 @@ echo
 	isoLabel='arconet-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 80-1"
+	archisoRequiredVersion="archiso 81-1"
 	buildFolder=$HOME"/arconet-build"
 	outFolder=$HOME"/arconet-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
