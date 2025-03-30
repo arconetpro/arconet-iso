728c2efcb486263885a77ac8af1c5bb47ed6778a
Erik Dubois <erik.dubois@gmail.com>
2025-03-30 07:13:17 +0200

    archiso

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -146,7 +146,7 @@ echo
 	isoLabel='arconet-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 82-1"
+	archisoRequiredVersion="archiso 83-1"
 	buildFolder=$HOME"/arconet-build"
 	outFolder=$HOME"/arconet-Out"
 
